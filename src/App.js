54c86a515a105e18e1a5531d60befff2ba215b8f
Flavia Pittet <pittet.flavia@gmail.com>
2018-11-24 16:52:53 +0100

    started css and main components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Timeline from './components/Timeline'
+import CustomMap from './components/CustomMap'
+import ReleasedGames from './components/ReleasedGames'
+import Charts from './components/Charts'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
- 
+      <div class="header">
+        <h1>Video games and crime</h1>
+        <p>lorem ipsum __</p>
+      </div>
+        <Timeline />
+        <CustomMap />
+        <ReleasedGames />
+        <Charts />
       </div>
     );
   }
